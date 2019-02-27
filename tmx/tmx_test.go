9589b763262efc6b60b0bbffd01b4b20997a3c47
Ben Cragg <bcvery1@gmail.com>
2019-02-27 11:35:59 +0000

    Added ReadFile tests

--- tmx/tmx_test.go
+++ tmx/tmx_test.go
@@ -7,6 +7,64 @@ import (
 	"github.com/bcvery1/tilepix/tmx"
 )
 
+func TestReadFile(t *testing.T) {
+	tests := []struct{
+		name string
+		filepath string
+		want *tmx.Map
+		wantErr bool
+	}{
+		{
+			name: "base64",
+			filepath: "testdata/base64.tmx",
+			want: nil,
+			wantErr: false,
+		},
+		{
+			name: "base64-zlib",
+			filepath: "testdata/base64-zlib.tmx",
+			want: nil,
+			wantErr: false,
+		},
+		{
+			name: "base64-gzip",
+			filepath: "testdata/base64-gzip.tmx",
+			want: nil,
+			wantErr: false,
+		},
+		{
+			name: "csv",
+			filepath: "testdata/csv.tmx",
+			want: nil,
+			wantErr: false,
+		},
+		{
+			name: "xml",
+			filepath: "testdata/xml.tmx",
+			want: nil,
+			wantErr: false,
+		},
+		{
+			name: "missing file",
+			filepath: "testdata/foo.tmx",
+			want: nil,
+			wantErr: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			_, err := tmx.ReadFile(tt.filepath)
+
+			if !tt.wantErr && err != nil {
+				t.Errorf("tmx.ReadFile(): got unexpected error: %v", err)
+			}
+			if tt.wantErr && err == nil {
+				t.Errorf("tmx.ReadFile(): expected error but not nil")
+			}
+		})
+	}
+}
+
 func TestProperties(t *testing.T) {
 	t.Log("Reading", "testdata/poly.tmx")
 
