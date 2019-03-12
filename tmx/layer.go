45aa82ebf945e28991a26c452380495cdfb36e21
Ben Cragg <bcvery1@gmail.com>
2019-03-12 11:28:21 +0000

    refactoring

--- tmx/layer.go
+++ tmx/layer.go
@@ -1,16 +1,83 @@
 package tmx
 
 type Layer struct {
-	Name         string         `xml:"name,attr"`
-	Opacity      float32        `xml:"opacity,attr"`
-	Visible      bool           `xml:"visible,attr"`
-	Properties   []Property     `xml:"properties>property"`
-	Data         Data           `xml:"data"`
+	Name       string     `xml:"name,attr"`
+	Opacity    float32    `xml:"opacity,attr"`
+	Visible    bool       `xml:"visible,attr"`
+	Properties []Property `xml:"properties>property"`
+	Data       Data       `xml:"data"`
 	// DecodedTiles is the attribute you should use instead of `Data`.
 	// Tile entry at (x,y) is obtained using l.DecodedTiles[y*map.Width+x].
 	DecodedTiles []*DecodedTile
 	// Tileset is only set when the layer uses a single tileset and NilLayer is false.
-	Tileset      *Tileset
+	Tileset *Tileset
 	// Empty should be set when all entries of the layer are NilTile.
-	Empty        bool
+	Empty bool
+}
+
+func (l *Layer) decode(width, height int) ([]GID, error) {
+	switch l.Data.Encoding {
+	case "csv":
+		return l.decodeLayerCSV(width, height)
+	case "base64":
+		return l.decodeLayerBase64(width, height)
+	case "":
+		// XML "encoding"
+		return l.decodeLayerXML(width, height)
+	}
+	return nil, UnknownEncodingError
+}
+
+func (l *Layer) decodeLayerXML(width, height int) ([]GID, error) {
+	if len(l.Data.DataTiles) != width*height {
+		return nil, InvalidDecodedDataLenError
+	}
+
+	gids := make([]GID, len(l.Data.DataTiles))
+	for i := 0; i < len(gids); i++ {
+		gids[i] = l.Data.DataTiles[i].GID
+	}
+
+	return gids, nil
+}
+
+func (l *Layer) decodeLayerCSV(width, height int) ([]GID, error) {
+	gids, err := l.Data.decodeCSV()
+	if err != nil {
+		return nil, err
+	}
+
+	if len(gids) != width*height {
+		return nil, InvalidDecodedDataLenError
+	}
+
+	return gids, nil
+}
+
+func (l *Layer) decodeLayerBase64(width, height int) ([]GID, error) {
+	dataBytes, err := l.Data.decodeBase64()
+	if err != nil {
+		return nil, err
+	}
+
+	if len(dataBytes) != width*height*4 {
+		return nil, InvalidDecodedDataLenError
+	}
+
+	gids := make([]GID, width*height)
+
+	j := 0
+	for y := 0; y < height; y++ {
+		for x := 0; x < width; x++ {
+			gid := GID(dataBytes[j]) +
+				GID(dataBytes[j+1])<<8 +
+				GID(dataBytes[j+2])<<16 +
+				GID(dataBytes[j+3])<<24
+			j += 4
+
+			gids[y*width+x] = gid
+		}
+	}
+
+	return gids, nil
 }
--- tmx/map.go
+++ tmx/map.go
@@ -13,19 +13,6 @@ type Map struct {
 	ObjectGroups []ObjectGroup `xml:"objectgroup"`
 }
 
-func (m *Map) decodeLayerXML(l *Layer) ([]GID, error) {
-	if len(l.Data.DataTiles) != m.Width*m.Height {
-		return []GID{}, InvalidDecodedDataLenError
-	}
-
-	gids := make([]GID, len(l.Data.DataTiles))
-	for i := 0; i < len(gids); i++ {
-		gids[i] = l.Data.DataTiles[i].GID
-	}
-
-	return gids, nil
-}
-
 func (m *Map) DecodeGID(gid GID) (*DecodedTile, error) {
 	if gid == 0 {
 		return NilTile, nil
@@ -49,63 +36,9 @@ func (m *Map) DecodeGID(gid GID) (*DecodedTile, error) {
 	return nil, InvalidGIDError
 }
 
-func (m *Map) decodeLayerCSV(l *Layer) ([]GID, error) {
-	gids, err := l.Data.decodeCSV()
-	if err != nil {
-		return []GID{}, err
-	}
-
-	if len(gids) != m.Width*m.Height {
-		return []GID{}, InvalidDecodedDataLenError
-	}
-
-	return gids, nil
-}
-
-func (m *Map) decodeLayerBase64(l *Layer) ([]GID, error) {
-	dataBytes, err := l.Data.decodeBase64()
-	if err != nil {
-		return []GID{}, err
-	}
-
-	if len(dataBytes) != m.Width*m.Height*4 {
-		return []GID{}, InvalidDecodedDataLenError
-	}
-
-	gids := make([]GID, m.Width*m.Height)
-
-	j := 0
-	for y := 0; y < m.Height; y++ {
-		for x := 0; x < m.Width; x++ {
-			gid := GID(dataBytes[j]) +
-				GID(dataBytes[j+1])<<8 +
-				GID(dataBytes[j+2])<<16 +
-				GID(dataBytes[j+3])<<24
-			j += 4
-
-			gids[y*m.Width+x] = gid
-		}
-	}
-
-	return gids, nil
-}
-
-func (m *Map) decodeLayer(l *Layer) ([]GID, error) {
-	switch l.Data.Encoding {
-	case "csv":
-		return m.decodeLayerCSV(l)
-	case "base64":
-		return m.decodeLayerBase64(l)
-	case "": // XML "encoding"
-		return m.decodeLayerXML(l)
-	}
-	return []GID{}, UnknownEncodingError
-}
-
 func (m *Map) decodeLayers() error {
-	for i := 0; i < len(m.Layers); i++ {
-		l := &m.Layers[i]
-		gids, err := m.decodeLayer(l)
+	for _, l := range m.Layers {
+		gids, err := l.decode(m.Width, m.Height)
 		if err != nil {
 			return err
 		}
