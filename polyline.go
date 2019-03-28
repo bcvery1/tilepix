abf5e8841b0353b4500cf6607646a803b966b7d4
Ben Cragg <bcvery1@gmail.com>
2019-03-28 11:58:39 +0000

    Correctly decoding and flipping points

--- polyline.go
+++ polyline.go
@@ -1,6 +1,10 @@
 package tilepix
 
-import "fmt"
+import (
+	"fmt"
+
+	log "github.com/sirupsen/logrus"
+)
 
 /*
   ___     _      _ _
@@ -22,7 +26,16 @@ type PolyLine struct {
 
 // Decode will return a slice of points which make up this polyline.
 func (p *PolyLine) Decode() ([]*Point, error) {
-	return decodePoints(p.Points)
+	if p.decodedPoints == nil {
+		dp, err := decodePoints(p.Points)
+		if err != nil {
+			log.WithError(err).Error("PolyLine.Decode: could not decode points")
+			return nil, err
+		}
+
+		p.decodedPoints = dp
+	}
+	return p.decodedPoints, nil
 }
 
 func (p *PolyLine) String() string {
@@ -32,7 +45,17 @@ func (p *PolyLine) String() string {
 func (p *PolyLine) setParent(m *Map) {
 	p.parentMap = m
 
+	// Must decode points before they can be set
+	_, err := p.Decode()
+	if err != nil {
+		log.WithError(err).Error("PolyLine.setParent: could not decode points")
+		return
+	}
+
 	for _, dp := range p.decodedPoints {
 		dp.setParent(m)
+
+		// We have to flip the Y co-ordinate here because the `tilepix.Point` is only used to provide `pixel.Vec`s
+		dp.flipY()
 	}
 }
