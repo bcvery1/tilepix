15cbed26313a083fe668f365540f2f0a3ddd0fa5
TooHTooH <thomas.lacroix@foundry.com>
2019-04-05 14:14:40 +0100

    Add tests for Centre and bounds

--- map_test.go
+++ map_test.go
@@ -43,3 +43,31 @@ func TestGetObjectByName(t *testing.T) {
 		}
 	}
 }
+
+func TestBounds(t *testing.T) {
+	m, err := tilepix.ReadFile("testdata/poly.tmx")
+	if err != nil {
+		t.Fatal(err)
+	}
+	rect := m.Bounds()
+	if rect.H() != 256 {
+		t.Error("error height bound invalid")
+	}
+	if rect.W() != 256 {
+		t.Error("error width bound invalid")
+	}
+}
+
+func TestCentre(t *testing.T) {
+	m, err := tilepix.ReadFile("testdata/poly.tmx")
+	if err != nil {
+		t.Fatal(err)
+	}
+	centre := m.Centre()
+	if centre.X != 128 {
+		t.Error("error centre X invalid")
+	}
+	if centre.Y != 128 {
+		t.Error("error centre Y invalid")
+	}
+}
